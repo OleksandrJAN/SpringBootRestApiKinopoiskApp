6378c4c694434fddc823e6370ef5c41de1f788a8
Александр <medvedev.sasha2000@mail.ru>
2020-06-09 14:15:48 +0300

    Init project

---
+++ src/test/java/com/kinopoiskApp/restAPI/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinopoiskApp.restAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
