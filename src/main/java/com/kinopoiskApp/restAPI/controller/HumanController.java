6a407b9e68dc9a6d5d68f91975a3834059108607
Александр <medvedev.sasha2000@mail.ru>
2020-06-11 10:08:11 +0300

    Add get mapping for humans and human

--- src/main/java/com/kinopoiskApp/restAPI/controller/HumanController.java
+++ src/main/java/com/kinopoiskApp/restAPI/controller/HumanController.java
@@ -1,14 +1,17 @@
 package com.kinopoiskApp.restAPI.controller;
 
 import com.kinopoiskApp.restAPI.domain.Human;
+import com.kinopoiskApp.restAPI.dto.HumanDto;
 import com.kinopoiskApp.restAPI.service.HumanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class HumanController {
@@ -21,7 +24,20 @@ public class HumanController {
     }
 
     @GetMapping("humans")
-    public ResponseEntity<List<Human>> getHumans() {
-        return new ResponseEntity<>(humanService.getHumans(), HttpStatus.OK);
+    public ResponseEntity<List<HumanDto>> getHumans() {
+        List<Human> humans = humanService.getHumans();
+        List<HumanDto> humanDtos = humans.stream().map(
+                HumanDto::new
+        ).collect(Collectors.toList());
+        return new ResponseEntity<>(humanDtos, HttpStatus.OK);
+    }
+
+    @GetMapping("humans/{human:[\\d]+}")
+    public ResponseEntity<HumanDto> getHuman(@PathVariable Human human) {
+        if (human != null) {
+            HumanDto humanDto = new HumanDto(human);
+            return new ResponseEntity<>(humanDto, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
