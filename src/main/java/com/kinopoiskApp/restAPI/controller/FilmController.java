4bd04940608d1f83cc0695f194f35cadec4cb285
Александр <medvedev.sasha2000@mail.ru>
2020-06-11 10:00:58 +0300

    Add films/{film} endpoint

--- src/main/java/com/kinopoiskApp/restAPI/controller/FilmController.java
+++ src/main/java/com/kinopoiskApp/restAPI/controller/FilmController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -29,5 +30,14 @@ public class FilmController {
                 FilmDto::new
         ).collect(Collectors.toList());
         return new ResponseEntity<>(filmsDtos, HttpStatus.OK);
+    }
+
+    @GetMapping("films/{film:[\\d]+}")
+    public ResponseEntity<FilmDto> getFilm(@PathVariable Film film) {
+        if (film != null) {
+            FilmDto filmsDto = new FilmDto(film);
+            return new ResponseEntity<>(filmsDto, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
