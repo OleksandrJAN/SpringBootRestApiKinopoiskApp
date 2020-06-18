a55ea2569951d877aba2334f66c3429b0a030444
Александр <medvedev.sasha2000@mail.ru>
2020-06-18 10:13:07 +0300

    Add film filtering and sorting

--- src/main/java/com/kinopoiskApp/restAPI/controller/FilmController.java
+++ src/main/java/com/kinopoiskApp/restAPI/controller/FilmController.java
@@ -1,39 +1,54 @@
 package com.kinopoiskApp.restAPI.controller;
 
 import com.kinopoiskApp.restAPI.domain.Film;
+import com.kinopoiskApp.restAPI.domain.FilmSortType;
+import com.kinopoiskApp.restAPI.domain.Genre;
 import com.kinopoiskApp.restAPI.dto.FilmDto;
 import com.kinopoiskApp.restAPI.dto.HumanInfo;
 import com.kinopoiskApp.restAPI.service.FilmService;
+import com.kinopoiskApp.restAPI.service.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class FilmController {
 
     private final FilmService filmService;
+    private final GenreService genreService;
 
     @Autowired
-    public FilmController(FilmService filmService) {
+    public FilmController(FilmService filmService, GenreService genreService) {
         this.filmService = filmService;
+        this.genreService = genreService;
     }
 
     @GetMapping("films")
-    public ResponseEntity<List<FilmDto>> getFilms() {
-        List<Film> films = filmService.getFilms();
-        List<FilmDto> filmsDtos = films.stream().map(
-                FilmDto::new
-        ).collect(Collectors.toList());
-        return new ResponseEntity<>(filmsDtos, HttpStatus.OK);
+    public ResponseEntity<List<FilmDto>> getFilms(
+            @RequestParam FilmSortType sort,
+            @RequestParam(name = "country", required = false) String country,
+            @RequestParam(name = "genre", required = false) String genreName
+    ) {
+        Genre genre = genreService.getGenreByName(genreName);
+        List<FilmDto> films = filmService.getFilms(sort, country, genre);
+        return new ResponseEntity<>(films, HttpStatus.OK);
+    }
+
+    @GetMapping("films/countries")
+    public ResponseEntity<List<String>> getFilmsCountries() {
+        List<String> filmsCountries = filmService.getFilmsCountries();
+        return new ResponseEntity<>(filmsCountries, HttpStatus.OK);
+    }
+
+    @GetMapping("films/genres")
+    public ResponseEntity<List<String>> getFilmsGenres() {
+        List<String> filmsGenres = filmService.getFilmsGenres();
+        return new ResponseEntity<>(filmsGenres, HttpStatus.OK);
     }
 
     @GetMapping("films/{film:[\\d]+}")
--- src/main/java/com/kinopoiskApp/restAPI/service/FilmService.java
+++ src/main/java/com/kinopoiskApp/restAPI/service/FilmService.java
@@ -1,14 +1,21 @@
 package com.kinopoiskApp.restAPI.service;
 
-import com.kinopoiskApp.restAPI.domain.Film;
-import com.kinopoiskApp.restAPI.domain.Human;
-import com.kinopoiskApp.restAPI.domain.HumanRoleInFilm;
+import com.kinopoiskApp.restAPI.domain.*;
+import com.kinopoiskApp.restAPI.dto.FilmDto;
 import com.kinopoiskApp.restAPI.dto.HumanInfo;
 import com.kinopoiskApp.restAPI.repo.FilmRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
-import java.util.*;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @Service
 public class FilmService {
@@ -20,26 +27,69 @@ public class FilmService {
         this.filmRepo = filmRepo;
     }
 
-    public List<Film> getFilms() {
-        return filmRepo.findAll();
+    public List<FilmDto> getFilms(FilmSortType sortType, String country, Genre genre) {
+        List<Film> films = filmRepo.findAll();
+        Predicate<Film> filterByCountry = getFilmFilterByCountry(country);
+        Predicate<Film> filterByGenre = getFilmFilterByGenre(genre);
+        Comparator<Film> filmComparator = getFilmComparator(sortType);
+        return films.stream()
+                .filter(filterByCountry)
+                .filter(filterByGenre)
+                .sorted(filmComparator)
+                .map(FilmDto::new)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> getFilmsCountries() {
+        List<Film> films = filmRepo.findAll();
+        return films.stream().map(
+                Film::getCountry
+        ).distinct().sorted().collect(Collectors.toList());
+    }
+
+    public List<String> getFilmsGenres() {
+        List<Film> films = filmRepo.findAll();
+        return films.stream().flatMap(
+                film -> {
+                    Set<Genre> filmGenres = film.getFilmGenres();
+                    return filmGenres.stream().map(
+                            Genre::getName
+                    ).collect(Collectors.toSet()).stream();
+                }
+        ).distinct().sorted().collect(Collectors.toList());
     }
 
     public Map<String, List<HumanInfo>> getFilmCast(Film film) {
-        Map<String, List<HumanInfo>> filmCastMap = new HashMap<>();
         Set<HumanRoleInFilm> humanRoles = film.getHumanRoles();
-
-        humanRoles.forEach(
+        // return map with career name and list of humans
+        return humanRoles.stream().map(
                 humanRoleInFilm -> {
                     Human human = humanRoleInFilm.getHuman();
                     String careerName = humanRoleInFilm.getCareer().getName();
-                    if (!filmCastMap.containsKey(careerName)) {
-                        filmCastMap.put(careerName, new ArrayList<>());
-                    }
-                    filmCastMap.get(careerName).add(new HumanInfo(human));
+                    // return pair with film's career name and human
+                    return new SimpleEntry<>(careerName, new HumanInfo(human));
                 }
-        );
+        ).collect(Collectors.groupingBy(Entry::getKey, Collectors.mapping(Entry::getValue, Collectors.toList())));
+    }
 
-        return filmCastMap;
+    private Predicate<Film> getFilmFilterByCountry(String country) {
+        return film -> StringUtils.isEmpty(country) || film.getCountry().equals(country);
+    }
+
+    private Predicate<Film> getFilmFilterByGenre(Genre genre) {
+        return film -> genre == null || film.getFilmGenres().contains(genre);
+    }
+
+    public Comparator<Film> getFilmComparator(FilmSortType filmSortType) {
+        return Comparator.comparing(film -> {
+            switch (filmSortType) {
+                case byName:
+                    return film.getFilmName();
+                case byYear:
+                default:
+                    return String.valueOf(film.getYear());
+            }
+        });
     }
 
 }
