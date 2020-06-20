951b141d0661498c4d749194817c5817ddf2998d
Александр <medvedev.sasha2000@mail.ru>
2020-06-20 18:57:40 +0300

    Add find by name method

--- src/main/java/com/kinopoiskApp/restAPI/repo/CareerRepo.java
+++ src/main/java/com/kinopoiskApp/restAPI/repo/CareerRepo.java
@@ -4,4 +4,5 @@ import com.kinopoiskApp.restAPI.domain.Career;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CareerRepo extends JpaRepository<Career, Long> {
+    Career findByName(String name);
 }
