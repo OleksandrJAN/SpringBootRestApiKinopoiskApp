8aaed13bcc6da0028842835a931ae0352e3c6316
Александр <medvedev.sasha2000@mail.ru>
2020-06-20 18:58:20 +0300

    Add equals and hashcode

--- src/main/java/com/kinopoiskApp/restAPI/dto/GenreInfo.java
+++ src/main/java/com/kinopoiskApp/restAPI/dto/GenreInfo.java
@@ -1,11 +1,13 @@
 package com.kinopoiskApp.restAPI.dto;
 
 import com.kinopoiskApp.restAPI.domain.Genre;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 public class GenreInfo {
 
     private Long id;
