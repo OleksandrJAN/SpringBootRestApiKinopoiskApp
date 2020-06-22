261125be86df5d096f3129713dfebb789b59e32f
Александр <medvedev.sasha2000@mail.ru>
2020-06-22 12:48:55 +0300

    Refactoring + remove useless endpoints + add filtering by film's country

--- src/main/java/com/kinopoiskApp/restAPI/controller/HumanController.java
+++ src/main/java/com/kinopoiskApp/restAPI/controller/HumanController.java
@@ -1,21 +1,19 @@
 package com.kinopoiskApp.restAPI.controller;
 
 import com.kinopoiskApp.restAPI.domain.*;
-import com.kinopoiskApp.restAPI.dto.FilmInfo;
 import com.kinopoiskApp.restAPI.dto.HumanDto;
-import com.kinopoiskApp.restAPI.service.CareerService;
-import com.kinopoiskApp.restAPI.service.GenreService;
-import com.kinopoiskApp.restAPI.service.HumanService;
-import com.kinopoiskApp.restAPI.service.SortService;
+import com.kinopoiskApp.restAPI.dto.factory.HumanDtoFactory;
+import com.kinopoiskApp.restAPI.dto.links.FilmInfo;
+import com.kinopoiskApp.restAPI.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -27,19 +25,19 @@ public class HumanController {
     private final HumanService humanService;
     private final GenreService genreService;
     private final CareerService careerService;
-    private final SortService sortService;
+    private final FilmService filmService;
 
     @Autowired
     public HumanController(
             HumanService humanService,
             GenreService genreService,
             CareerService careerService,
-            SortService sortService
+            FilmService filmService
     ) {
         this.humanService = humanService;
         this.genreService = genreService;
         this.careerService = careerService;
-        this.sortService = sortService;
+        this.filmService = filmService;
     }
 
 
@@ -53,80 +51,78 @@ public class HumanController {
         // filtering by career if needed
         Career career = careerService.getCareerByName(careerName);
         if (career != null) {
-            Predicate<Human> humansFilterByCareer = sortService.getHumansFilterByCareer(career);
+            Predicate<Human> humansFilterByCareer = SortService.getHumansFilterByCareer(career);
             humans = humans.filter(humansFilterByCareer);
         }
         // filtering by genre if needed
         Genre genre = genreService.getGenreByName(genreName);
         if (genre != null) {
-            Predicate<Human> humansFilterByGenre = sortService.getHumansFilterByGenre(genre);
+            Predicate<Human> humansFilterByGenre = SortService.getHumansFilterByGenre(genre);
             humans = humans.filter(humansFilterByGenre);
         }
         // sorting by HumanSortType
-        Comparator<Human> humansComparator = sortService.getHumansComparator(sortType);
+        Comparator<Human> humansComparator = SortService.getHumansComparator(sortType);
         humans = humans.sorted(humansComparator);
         // cast Human to HumanDto
         List<HumanDto> humanDtos = humanService.getHumansDtos(humans).collect(Collectors.toList());
         return new ResponseEntity<>(humanDtos, HttpStatus.OK);
     }
 
-    @GetMapping("humans/careers")
-    public ResponseEntity<List<String>> getHumansCareers() {
-        Stream<Human> humans = humanService.getHumans();
-        Set<Career> humansCareers = humanService.getHumansCareers(humans);
-        List<String> careersNames = careerService.getSortedCareersNames(humansCareers);
-        return new ResponseEntity<>(careersNames, HttpStatus.OK);
-    }
-
-    @GetMapping("humans/genres")
-    public ResponseEntity<List<String>> getHumansGenres() {
-        Stream<Human> humans = humanService.getHumans();
-        Set<Genre> humansGenres = humanService.getHumansGenres(humans);
-        List<String> genresNames = genreService.getSortedGenresNames(humansGenres);
-        return new ResponseEntity<>(genresNames, HttpStatus.OK);
-    }
-
 
     @GetMapping("humans/{human:[\\d]+}")
     public ResponseEntity<HumanDto> getHuman(@PathVariable Human human) {
         if (human != null) {
-            HumanDto humanDto = new HumanDto(human);
+            HumanDto humanDto = HumanDtoFactory.createHumanDto(human);
             return new ResponseEntity<>(humanDto, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @GetMapping("humans/{human:[\\d]+}/roles")
-    public ResponseEntity<Map<String, List<FilmInfo>>> getHumanRoles(
+    @GetMapping("humans/{human:[\\d]+}/films")
+    public ResponseEntity<Map<String, List<FilmInfo>>> getHumanFilms(
             @PathVariable Human human,
             @RequestParam(name = "sort") FilmSortType sortType,
+            @RequestParam(name = "country", required = false) String country,
             @RequestParam(name = "genre", required = false) String genre
     ) {
         if (human != null) {
-            Set<Film> humanFilms = humanService.getHumanFilms(human);
-            Stream<Film> filmStream = humanFilms.stream();
-
+            Stream<Film> films = humanService.getHumanFilms(human);
+            // filtering by country if needed
+            if (!StringUtils.isEmpty(country)) {
+                Predicate<Film> filmsFilterByCountry = SortService.getFilmsFilterByCountry(country);
+                films = films.filter(filmsFilterByCountry);
+            }
+            // filtering by genre if needed
             Genre filteringGenre = genreService.getGenreByName(genre);
             if (filteringGenre != null) {
-                Predicate<Film> filmsFilterByGenre = sortService.getFilmsFilterByGenre(filteringGenre);
-                filmStream = filmStream.filter(filmsFilterByGenre);
+                Predicate<Film> filmsFilterByGenre = SortService.getFilmsFilterByGenre(filteringGenre);
+                films = films.filter(filmsFilterByGenre);
             }
-
-            Comparator<Film> filmsComparator = sortService.getFilmsComparator(sortType);
-            filmStream = filmStream.sorted(filmsComparator);
-
-
-            Map<String, List<FilmInfo>> humanRolesInFilms = humanService.getHumanCareersWithFilmsMap(human, filmStream);
+            // sorting by sortType
+            Comparator<Film> filmsComparator = SortService.getFilmsComparator(sortType);
+            films = films.sorted(filmsComparator);
+            // get map with <career name, list of films with career name>
+            Map<String, List<FilmInfo>> humanRolesInFilms = humanService.getHumanCareersWithFilmsMap(human, films);
             return new ResponseEntity<>(humanRolesInFilms, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
-    @GetMapping("humans/{human:[\\d]+}/genres")
-    public ResponseEntity<List<String>> getHumanGenres(@PathVariable Human human) {
+    @GetMapping("humans/{human:[\\d]+}/films-countries")
+    public ResponseEntity<List<String>> getHumanFilmsCountries(@PathVariable Human human) {
         if (human != null) {
-            Set<Genre> humanGenres = humanService.getHumanGenres(human);
-            List<String> genresNames = genreService.getSortedGenresNames(humanGenres);
+            Stream<Film> humanFilms = humanService.getHumanFilms(human);
+            List<String> filmsCountries = filmService.getFilmsCountries(humanFilms);
+            return new ResponseEntity<>(filmsCountries, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("humans/{human:[\\d]+}/films-genres")
+    public ResponseEntity<List<String>> getHumanFilmsGenres(@PathVariable Human human) {
+        if (human != null) {
+            Stream<Genre> humanGenres = humanService.getHumanGenres(human);
+            List<String> genresNames = genreService.getGenresNames(humanGenres);
             return new ResponseEntity<>(genresNames, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
