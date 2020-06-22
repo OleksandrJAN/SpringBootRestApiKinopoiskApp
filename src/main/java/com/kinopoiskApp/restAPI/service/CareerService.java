c04d22f32889282508e1ce3005cc7410810e61f8
Александр <medvedev.sasha2000@mail.ru>
2020-06-22 12:43:56 +0300

    Remove useless methods

--- src/main/java/com/kinopoiskApp/restAPI/service/CareerService.java
+++ src/main/java/com/kinopoiskApp/restAPI/service/CareerService.java
@@ -1,13 +1,16 @@
 package com.kinopoiskApp.restAPI.service;
 
 import com.kinopoiskApp.restAPI.domain.Career;
+import com.kinopoiskApp.restAPI.domain.Film;
+import com.kinopoiskApp.restAPI.domain.Human;
+import com.kinopoiskApp.restAPI.domain.HumanRoleInFilm;
 import com.kinopoiskApp.restAPI.repo.CareerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class CareerService {
@@ -19,17 +22,26 @@ public class CareerService {
         this.careerRepo = careerRepo;
     }
 
-    public List<Career> getCareers() {
-        return careerRepo.findAll();
-    }
 
-    public List<String> getSortedCareersNames(Collection<Career> careers) {
-        return careers.stream().map(
-                Career::getName
-        ).sorted().collect(Collectors.toList());
+    public Stream<Career> getCareers() {
+        return careerRepo.findAll().stream();
     }
 
     public Career getCareerByName(String careerName) {
         return careerRepo.findByName(careerName);
     }
+
+    public List<String> getCareersNames(Stream<Career> careers) {
+        return careers
+                .map(Career::getName)
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    public Stream<Career> getHumanCareersInFilm(Human human, Film film) {
+        return film.getHumanRoles().stream()
+                .filter(humanRoleInFilm -> humanRoleInFilm.getHuman().equals(human))
+                .map(HumanRoleInFilm::getCareer);
+    }
+
 }
--- src/main/java/com/kinopoiskApp/restAPI/service/GenreService.java
+++ src/main/java/com/kinopoiskApp/restAPI/service/GenreService.java
@@ -1,18 +1,13 @@
 package com.kinopoiskApp.restAPI.service;
 
-import com.kinopoiskApp.restAPI.domain.Film;
 import com.kinopoiskApp.restAPI.domain.Genre;
-import com.kinopoiskApp.restAPI.domain.Human;
-import com.kinopoiskApp.restAPI.domain.HumanRoleInFilm;
 import com.kinopoiskApp.restAPI.repo.GenreRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class GenreService {
@@ -24,40 +19,20 @@ public class GenreService {
         this.genreRepo = genreRepo;
     }
 
-    public List<Genre> getGenres() {
-        return genreRepo.findAll();
+
+    public Stream<Genre> getGenres() {
+        return genreRepo.findAll().stream();
     }
 
+    public Genre getGenreByName(String genreName) {
+        return genreRepo.findByName(genreName);
+    }
 
-    public List<Human> getHumanListSortedByCountGenreMatches(Genre genre) {
-        //мапа с количеством совпадений в жанре для человека
-        Map<Human, Integer> humanMapWithGenreMatches = new HashMap<>();
-
-        //все фильмы жанра genre
-        Set<Film> filmsWithGenre = genre.getFilms();
-        for (Film film : filmsWithGenre) {
-            //для каждого фильма из filmsWithGenre жанра genre
-            //получаю всех людей и помещаю во множество humans
-            Set<HumanRoleInFilm> humanRoles = film.getHumanRoles();
-            Set<Human> humans = humanRoles.stream().map(
-                    HumanRoleInFilm::getHuman
-            ).collect(Collectors.toSet());
-
-            //каждого человека закидываю в мапу
-            for (Human human : humans) {
-                Integer count = humanMapWithGenreMatches.get(human);
-                humanMapWithGenreMatches.put(human, (count == null) ? 1 : count + 1);
-            }
-        }
-
-        //сортирую мапу по убыванию количества совпадений с жанром и возвращаю остортированный список людей
-        //обязательно список т.к. сортировка
-        //проверить сортировку на правильность!!!!!!
-        return humanMapWithGenreMatches.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue())
-                .map(Map.Entry::getKey)
+    public List<String> getGenresNames(Stream<Genre> genres) {
+        return genres
+                .map(Genre::getName)
+                .sorted()
                 .collect(Collectors.toList());
     }
-
 
 }
