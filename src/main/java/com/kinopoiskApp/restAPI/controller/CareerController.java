0de6267b84911b89d553115bb38a77b35a8f9f98
Александр <medvedev.sasha2000@mail.ru>
2020-06-22 12:50:47 +0300

    Remove CareerDto

--- src/main/java/com/kinopoiskApp/restAPI/controller/CareerController.java
+++ src/main/java/com/kinopoiskApp/restAPI/controller/CareerController.java
@@ -1,16 +1,19 @@
 package com.kinopoiskApp.restAPI.controller;
 
 import com.kinopoiskApp.restAPI.domain.Career;
-import com.kinopoiskApp.restAPI.dto.CareerDto;
 import com.kinopoiskApp.restAPI.service.CareerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class CareerController {
 
     private final CareerService careerService;
@@ -20,12 +23,11 @@ public class CareerController {
         this.careerService = careerService;
     }
 
-    @GetMapping("/careers/{career:[\\d]+}")
-    public ResponseEntity<CareerDto> getCareer(@PathVariable Career career) {
-        if (career != null) {
-            CareerDto careerDto = new CareerDto(career);
-            return new ResponseEntity<>(careerDto, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @GetMapping("careers")
+    public ResponseEntity<List<String>> getCareers() {
+        Stream<Career> careers = careerService.getCareers();
+        List<String> careersNames = careerService.getCareersNames(careers);
+        return new ResponseEntity<>(careersNames, HttpStatus.OK);
     }
+
 }
